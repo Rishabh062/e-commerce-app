84d129bbba5092f1202472eb7b4f068badce5ba2
Rishabh Dwivedi <rishabhdwivedi062@gmail.com>
2023-12-28 00:22:49 +0530

    Added Hero section and Nav bar component

---
+++ src/components/MaxWidthWrapper.tsx
@@ -0,0 +1,16 @@
+import { cn } from '@/lib/utils'
+import React, { ReactNode } from 'react'
+
+interface maxWidthWrapperProps {
+    className?: string,
+    children: ReactNode
+}
+const MaxWidthWrapper = ({ className, children }: maxWidthWrapperProps) => {
+    return (
+        <div className={cn("mx-auto w-full max-w-screen-xl px-2.5 md:px-20", className)}>
+            {children}
+        </div>
+    )
+}
+
+export default MaxWidthWrapper
